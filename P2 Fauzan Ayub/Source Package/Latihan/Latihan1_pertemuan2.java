b1a1fc757de96a789bc2b6adf742c1bfcff007b0
Fauzan Ayub <132631777+ZannBaeRyou@users.noreply.github.com>
2026-04-06 20:35:51 +0700

    Add files via upload

---
+++ P2 Fauzan Ayub/Source Package/Latihan/Latihan1_pertemuan2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Latihan1_pertemuan2 
+{
+    public static void main(String args[])
+    {
+    Scanner input = new Scanner(System.in);
+    String nama, nim, address;
+
+    System.out.print("input nama: ");
+    nama = input.nextLine();
+
+    System.out.print("input nim: ");
+    nim = input.nextLine();
+    
+    System.out.print("input address: ");
+    address = input.nextLine();
+    
+    System.out.println("Nama: " + nama);
+    System.out.println("NIM: " + nim);
+    System.out.println("Address: " + address);
+    }
+} 
+    
+
